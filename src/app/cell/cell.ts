a0d4cac8bb31730acce2531136769573c230f299
Uzi Landsmann <uzi.landsmann@arbetsformedlingen.se>
2017-01-30 08:56:12 +0100

    Back to using tables and some more css

--- src/app/cell/cell.ts
+++ src/app/cell/cell.ts
@@ -1,4 +1,17 @@
 export class Cell {
 
-  public value: number = 0;
+  private value: number = 0;
+  private possibles: Array<number> = [];
+
+  constructor() {
+    this.resetCell();
+  }
+
+  private resetCell() {
+    this.value = 0;
+    for (let x = 0; x < 9; x++) {
+      this.possibles[x] = x;
+    }
+  }
+
 }
